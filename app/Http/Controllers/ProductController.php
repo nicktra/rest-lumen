83c3b2b62e4de6968f972a3fd58b24531aa842b5
baloh <balohbaloh@yahoo.co.id>
2021-03-05 21:21:38 +0700

    CRUD code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,6 +7,21 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function index()
+    {
+        $product = Product::all();
+
+        return response()->json($product);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        return response()->json($product);
+    }
+
     public function create(Request $request)
     {
         $this->validate($request, [
@@ -21,5 +36,42 @@ class ProductController extends Controller
         $product = Product::create($data);
 
         return response()->json($product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        if(!$product) {
+            return response()->json(['message' => 'Product Not Found'], 404);
+        }
+
+        $this->validate($request, [
+            'name' => 'string',
+            'price' => 'integer',
+            'color' => 'string',
+            'condition' => 'in:new,old',
+            'description' => 'string'
+        ]);
+
+        $data = $request->all();
+
+        $product->fill($data);
+
+        $product->save();
+
+        return response()->json($product);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+
+        if(!$product) {
+            return response()->json(['message' => 'Product Not Found'], 404);
+        }
+
+        $product->delete();
+        return response()->json(['message' => 'Product Deleted!']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 */
 
 $router->post('/product', 'ProductController@create');
+$router->get('/product', 'ProductController@index');
+$router->get('/product/{id}', 'ProductController@show');
+$router->put('/product/{id}', 'ProductController@update');
+$router->delete('/product/{id}', 'ProductController@destroy');
